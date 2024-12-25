eaff189be926c94c57b8b414b0b51ccef8c48aab
Lel Illumina <dragonbilal938@outlook.com>
2024-12-25 20:19:58 +0500

    refactor: switch to default exports

--- src/commands/utility/ping.ts
+++ src/commands/utility/ping.ts
@@ -1,10 +1,11 @@
 import { SlashCommandBuilder } from "discord.js";
 import { CommandInteraction } from "discord.js";
 
-export const data = new SlashCommandBuilder()
-  .setName("ping")
-  .setDescription("Replies with Pong!");
-
-export async function execute(interaction: CommandInteraction) {
-  await interaction.reply("Pong!");
-}
+export default {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!"),
+  async execute(interaction: CommandInteraction) {
+    await interaction.reply("Pong!");
+  },
+};
--- src/commands/utility/user.ts
+++ src/commands/utility/user.ts
@@ -1,14 +1,20 @@
 import { SlashCommandBuilder } from "discord.js";
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction, GuildMember } from "discord.js";
 
-export const data = new SlashCommandBuilder()
-  .setName("user")
-  .setDescription("Provides information about the user.");
-
-export async function execute(interaction: CommandInteraction) {
-  // interaction.user is the object representing the User who ran the command//-
-  // interaction.member is the GuildMember object, which represents the user in the specific guild//-
-  await interaction.reply(
-    `This command was run by ${interaction.user.username}, who joined on ${interaction.member?.joinedAt}.`
-  );
-}
+export default {
+  data: new SlashCommandBuilder()
+    .setName("user")
+    .setDescription("Provides information about the user."),
+  async execute(interaction: CommandInteraction) {
+    // Check if interaction.member is a GuildMember
+    if (interaction.member instanceof GuildMember) {
+      await interaction.reply(
+        `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`
+      );
+    } else {
+      await interaction.reply(
+        `This command was run by ${interaction.user.username}, but we couldn't retrieve the join date.`
+      );
+    }
+  },
+};
