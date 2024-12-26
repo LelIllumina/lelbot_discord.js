97f896d2ae509f3aec3d47009b186fbc524d7089
Lel Illumina <dragonbilal938@outlook.com>
2024-12-27 03:34:02 +0500

    refactor: promised cunny

--- src/events/cunnyreact.ts
+++ src/events/cunnyreact.ts
@@ -5,14 +5,13 @@ export default {
   async execute(message: Message) {
     const keywords = ["cunny", "cnnuy", "cnuuy"];
     if (keywords.some((word) => message.content.toLowerCase().includes(word))) {
-      // Replace 'specificword' with your target word
-      try {
-        await message.react("😭");
-        await message.react("💢");
-        await message.react("<:KaguyaSob:1102331479066935316>");
-      } catch (error) {
-        console.error(error);
-      }
+      Promise.all([
+        message.react("😭"),
+        message.react("💢"),
+        message.react("<:KaguyaSob:1102331479066935316>"),
+      ]).catch((error) =>
+        console.error("One of the emojis failed to react:", error)
+      );
     }
   },
 };
